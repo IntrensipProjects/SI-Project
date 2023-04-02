e27efa593eebb26dc4dc0731ef41ea878025c314
unknown <mano497>
2023-04-02 15:31:50 +0000

    commit branch 1

--- employee-service/src/main/java/org/sid/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/org/sid/employeeservice/EmployeeServiceApplication.java
@@ -17,29 +17,23 @@ public class EmployeeServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmployeeServiceApplication.class, args);
     }
-    @Bean
-    CommandLineRunner start(EmployeeRepository employeeRepository,
-                            RepositoryRestConfiguration restConfiguration){
-        return args -> {
-            restConfiguration.exposeIdsFor(Employee.class);
-//            Employee employee = new Employee();
-//            employee.setNomComplet("Nouhaila OHAPOUNE");
-//            employee.setAdresse("Casablanca");
-//            employee.setDateDeNaissance(new Date());
-//            employee.setCNE("BA2596");
-//            employee.setPositionHeld("Dev");
-//            employee.setTel("0644359316");
 
-            employeeRepository.saveAll(
-                    List.of(
-                            Employee.builder().nomComplet("Nouhaila OHAPOUNE").adresse("Casablanca").CNE("BA2596")
-                                    .email("n.ohapoune@mundiapolis.ma").positionHeld("dev").dateDeNaissance("07/01/2000")
-                                    .tel("0644359316").dateEmbauche("03/06/2024").build()
-
-                    )
-            );
-        };
-    }
+//    @Bean
+//    CommandLineRunner start(EmployeeRepository employeeRepository,
+//                            RepositoryRestConfiguration restConfiguration){
+//        return args -> {
+//            restConfiguration.exposeIdsFor(Employee.class);
+//
+//            employeeRepository.saveAll(
+//                    List.of(
+//                            Employee.builder().nomComplet("Nouhaila OHAPOUNE").adresse("Casablanca").CNE("BA2596")
+//                                    .email("n.ohapoune@mundiapolis.ma").positionHeld("dev").dateDeNaissance("07/01/2000")
+//                                    .tel("0644359316").dateEmbauche("03/06/2024").build()
+//
+//                    )
+//            );
+//        };
+//    }
 
 
 
--- employee-service/src/main/java/org/sid/employeeservice/Repositories/EmployeeRepository.java
+++ employee-service/src/main/java/org/sid/employeeservice/Repositories/EmployeeRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface EmployeeRepository extends JpaRepository<Employee,Long > {
+
 }
+
