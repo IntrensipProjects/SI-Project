d78c6fec2b7921ea7a94556a0908c3d4a613e6e3
Nouhaila <you@example.com>
2023-05-29 11:11:40 +0100

    NO-frontend

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import LoginComponent from "./pages/LoginPage/components/LoginComponent/LoginComponent";
 import {Routes,Route} from "react-router-dom";
 import SignupComponent from "./pages/SignupPage/components/SignupComponent/SignupComponent";
-import CongratsComponent from "./components/CongratsComponent/CongratsComponent";
 import HomePage from "./pages/AccueilPage/HomePage";
+import CongratsPage from "./pages/CongratsPage";
 function App() {
   return (
       <>
@@ -11,7 +11,7 @@ function App() {
                     <Route path="/" element={<LoginComponent/>} />
                     <Route path="/signup" element={<SignupComponent/>} />
                     <Route path="/login" element={<LoginComponent/>} />
-                    <Route path="/congrats" element={<CongratsComponent/>} />
+                    <Route path="/congrats" element={<CongratsPage/>} />
                     <Route path="/home" element={<HomePage/>} />
 
                 </Routes>
